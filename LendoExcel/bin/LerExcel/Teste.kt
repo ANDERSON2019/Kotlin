160271530b9981d19894729bb237c52ae1ce5aa0
ANDERSON2019 <anderson.sousa2017@outlook.com>
2017-06-01 00:09:32 -0300

    Add files via upload
    Projeto

---
+++ LendoExcel/bin/LerExcel/Teste.kt
@@ -0,0 +1,11 @@
+package LerExcel
+
+fun main(args :Array<String>){
+	
+	
+	
+	
+}
+
+class Pessoa(val nome : String, val idade : Int){}
+
---
+++ LendoExcel/src/LerExcel/LerExcelUtil.kt
@@ -0,0 +1,19 @@
+package LerExcel
+
+class LerExcelUtil {
+	
+	val Nome = String 
+	val idade = Int
+	val cpf = Int
+	
+	
+	fun lerExcel(){
+		
+		
+		
+		
+		
+	}
+	
+	
+}
